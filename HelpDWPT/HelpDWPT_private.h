5491a1178351475d7926b39d450784476b1bfce0
Office-Excel <139354638+BOffice-Excel@users.noreply.github.com>
2024-06-01 11:06:30 +0800

    Add files via upload

--- HelpDWPT/HelpDWPT_private.h
+++ HelpDWPT/HelpDWPT_private.h
@@ -5,19 +5,19 @@
 #define HELPDWPT_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.0.0"
-#define VER_MAJOR	1
+#define VER_STRING	"0.0.3.0"
+#define VER_MAJOR	0
 #define VER_MINOR	0
-#define VER_RELEASE	0
+#define VER_RELEASE	3
 #define VER_BUILD	0
 #define COMPANY_NAME	""
-#define FILE_VERSION	"1.0.0.0"
+#define FILE_VERSION	"0.0.3.0"
 #define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
-#define INTERNAL_NAME	""
-#define LEGAL_COPYRIGHT	""
+#define INTERNAL_NAME	"DWPT∞Ô÷˙ø‚"
+#define LEGAL_COPYRIGHT	"∞Ê»®À˘”– Office-Excel"
 #define LEGAL_TRADEMARKS	""
-#define ORIGINAL_FILENAME	""
-#define PRODUCT_NAME	""
-#define PRODUCT_VERSION	"1.0.0.0"
+#define ORIGINAL_FILENAME	"HelpDWPT.dll"
+#define PRODUCT_NAME	"DWPT∞Ô÷˙ø‚"
+#define PRODUCT_VERSION	"0.0.3.0"
 
 #endif /*HELPDWPT_PRIVATE_H*/
